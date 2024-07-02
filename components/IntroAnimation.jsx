e789ebbf368534cb8aae8e66fe16fe971c326a34
Esteban Alban <esteban.alban9003@gmail.com>
2024-07-01 22:47:24 -0500

    changes to font v3

--- components/IntroAnimation.jsx
+++ components/IntroAnimation.jsx
@@ -39,21 +39,21 @@ const IntroAnimation = () => {
 
 	return (
 		<>
-			<div className="main-container w-full h-screen bg-[var(--zen-yellow)] font-custom fixed top-0 left-0 z-10"></div>
-			<p className="w-full text-left xl:pl-52 pl-10 fixed bottom-[55%] lg:bottom-[55%] left-[50%] translate-x-[-50%] translate-y-[50%] z-[11] font-customLight text-[50px] md:text-[80px] lg:text-[100px] 2xl:text-[120px] text-[var(--zen-blue)] font-bold mix-blend-plus-lighter">
+			<div className="main-container w-full h-screen bg-[var(--zen-yellow)] gSans fixed top-0 left-0 z-10"></div>
+			<p className="w-full text-left xl:pl-52 pl-10 fixed bottom-[55%] lg:bottom-[55%] left-[50%] translate-x-[-50%] translate-y-[50%] z-[11] gSansLight text-[50px] md:text-[80px] lg:text-[100px] 2xl:text-[120px] text-[var(--zen-blue)] font-bold mix-blend-plus-lighter">
 				El Bello
 			</p>
 			<div className="second-container fixed bg-[var(--zen-yellow)] bottom-[55%] lg:bottom-[55%] left-[50%] translate-x-[-50%] translate-y-[50%] z-[12] w-full lg:h-[120px] h-[80px]"></div>
-			<p className="w-full text-left xl:pl-52 pl-10 fixed bottom-[45%] lg:bottom-[40%] left-[50%] translate-x-[-50%] translate-y-[50%] z-[13] font-customLight text-[50px] md:text-[80px] lg:text-[100px] 2xl:text-[120px] text-[var(--zen-blue)] font-bold mix-blend-plus-lighter">
-				Arte de <span className="font-customMedium">Vivir</span>
+			<p className="w-full text-left xl:pl-52 pl-10 fixed bottom-[45%] lg:bottom-[40%] left-[50%] translate-x-[-50%] translate-y-[50%] z-[13] gSansLight text-[50px] md:text-[80px] lg:text-[100px] 2xl:text-[120px] text-[var(--zen-blue)] font-bold mix-blend-plus-lighter">
+				Arte de <span className="gSansMedium">Vivir</span>
 			</p>
 			<div className="second-container fixed bg-[var(--zen-yellow)] bottom-[45%] lg:bottom-[40%] left-[50%] translate-x-[-50%] translate-y-[50%] z-[14] w-full lg:h-[120px] h-[80px]"></div>
-			<p className="second-text w-full text-left xl:pl-52 pl-10 fixed bottom-[35%] lg:bottom-[25%] left-[50%] translate-x-[-50%] translate-y-[50%] z-[15] font-customLight text-[50px] md:text-[80px] lg:text-[100px] 2xl:text-[120px] text-[var(--zen-blue)] font-bold mix-blend-plus-lighter">
+			<p className="second-text w-full text-left xl:pl-52 pl-10 fixed bottom-[35%] lg:bottom-[25%] left-[50%] translate-x-[-50%] translate-y-[50%] z-[15] gSansLight text-[50px] md:text-[80px] lg:text-[100px] 2xl:text-[120px] text-[var(--zen-blue)] font-bold mix-blend-plus-lighter">
 				Para un
 			</p>
 			<div className="second-container fixed bg-[var(--zen-yellow)] bottom-[35%] lg:bottom-[25%] left-[50%] translate-x-[-50%] translate-y-[50%] z-[16] w-full lg:h-[120px] h-[80px]"></div>
-			<p className="second-text w-full text-left xl:pl-52 pl-10 fixed bottom-[25%] lg:bottom-[10%] left-[50%] translate-x-[-50%] translate-y-[50%] z-[17] font-customLight text-[50px] md:text-[80px] lg:text-[100px] 2xl:text-[120px] text-[var(--zen-blue)] font-bold mix-blend-plus-lighter">
-				Buen <span className="font-customMedium">Partir</span>
+			<p className="second-text w-full text-left xl:pl-52 pl-10 fixed bottom-[25%] lg:bottom-[10%] left-[50%] translate-x-[-50%] translate-y-[50%] z-[17] gSansLight text-[50px] md:text-[80px] lg:text-[100px] 2xl:text-[120px] text-[var(--zen-blue)] font-bold mix-blend-plus-lighter">
+				Buen <span className="gSansMedium">Partir</span>
 			</p>
 			<div className="second-container fixed bg-[var(--zen-yellow)] bottom-[25%] lg:bottom-[10%] left-[50%] translate-x-[-50%] translate-y-[50%] z-[18] w-full lg:h-[120px] h-[80px]"></div>
 			<div className="w-full h-screen fixed top-0 left-0 z-[9]">
