8881f142179affd670a10d86799df7465125526a
Esteban Alban <esteban.alban9003@gmail.com>
2024-07-20 21:10:18 -0500

    ui changes

--- components/IntroAnimation.jsx
+++ components/IntroAnimation.jsx
@@ -6,9 +6,9 @@ import CourseSection from "./CourseSection";
 import Lenis from "lenis";
 
 const IntroAnimation = () => {
-	// useEffect(() => {
-	// 	window.scrollTo(0, 0);
-	// }, []);
+	useEffect(() => {
+		window.scrollTo(0, 0);
+	}, []);
 
 	useEffect(() => {
 		const lenis = new Lenis();
@@ -26,7 +26,7 @@ const IntroAnimation = () => {
 			document
 				.getElementById("courses-container")
 				.scrollIntoView({ behavior: "smooth" });
-		}, 15000);
+		}, 10000);
 
 		return () => clearTimeout(timer);
 	}, []);
@@ -89,7 +89,7 @@ const IntroAnimation = () => {
 		<>
 			<div className="main-container w-full h-screen bg-[var(--zen-analog)] gSans fixed top-0 left-0 z-[4]"></div>
 			<div className="hero-container w-full h-screen relative bg-[var(--zen-comp)]">
-				<div className="slogan absolute z-[4] top-[60%] left-[5%] translate-y-[-50%] 2xl:text-[100px] xl:text-[60px] text-[40px] gSansLight font-bold text-[var(--zen-analog)] mix-blend-plus-lighter">
+				<div className="slogan absolute z-[4] top-[65%] left-[5%] translate-y-[-50%] 2xl:text-[100px] xl:text-[80px] text-[40px] gSansLight font-bold text-[var(--zen-analog)] mix-blend-plus-lighter">
 					<p className="first-text opacity-0">Tu Yo</p>
 					<p className="first-text opacity-0">
 						Del <span className="gSansMedium">Futuro</span>
@@ -99,20 +99,22 @@ const IntroAnimation = () => {
 						En este <span className="gSansMedium">Presente</span>
 					</p>
 				</div>
-				<div className="absolute z-[3] w-full h-full flex flex-col md:flex-row items-center justify-center 2xl:gap-[280px] xl:gap-[100px] gap-[10px]">
-					<Image
-						src="/Assets/Logo-Azul-Texto.webp"
-						alt="logo"
-						width={300}
-						height={400}
-						className="logo 2xl:w-[400px] md:w-[280px] w-[120px] opacity-0"
-					/>
-					<p className="third-text md:w-[700px] w-[360px] 2xl:text-[30px] text-[20px] text-center font-semibold text-[var(--zen-white)] p-[30px] opacity-0">
-						La primera escuela completamente enfocada en la realización
-						espiritual, basada en los ocho pilares primordiales de la flor
-						dorada según la filosofía del tao y las escuelas ancestrales fang
-						shi del yin-yang.
-					</p>
+				<div className="gSans absolute z-[3] w-full h-full flex items-center justify-center 2xl:p-[80px] xl:p-[50px] p-[20px]">
+					<div className="relative top-0 left-0 third-text w-full h-full flex flex-col md:flex-row items-center justify-center 2xl:gap-[80px] xl:gap-[100px] gap-[10px] bg-[#00000080] rounded-xl opacity-0">
+						<Image
+							src="/Assets/Logo-Azul-Texto.webp"
+							alt="logo"
+							width={300}
+							height={400}
+							className="logo 2xl:w-[250px] md:w-[200px] w-[120px] opacity-0"
+						/>
+						<p className="third-text md:w-[700px] w-[360px] 2xl:text-[26px] text-[18px] text-center font-semibold text-[var(--zen-white)] px-[60px] opacity-0">
+							La primera escuela completamente enfocada en la realización
+							espiritual, basada en los ocho pilares primordiales de la flor
+							dorada según la filosofía del tao y las escuelas ancestrales fang
+							shi del yin-yang.
+						</p>
+					</div>
 				</div>
 				<div className="w-full h-full absolute">
 					<video autoPlay muted loop className="w-full h-full object-cover">
