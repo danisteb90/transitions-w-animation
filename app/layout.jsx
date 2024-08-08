8ea179023c3e1b27b84862c78b8e92a3b6722567
Esteban Alban <esteban.alban9003@gmail.com>
2024-08-08 09:23:20 -0500

    changes to seo

--- app/layout.jsx
+++ app/layout.jsx
@@ -7,6 +7,20 @@ export const metadata = {
 	title: "Escuela de Libertad",
 	description:
 		"La primera Escuela de realización espiritual, basada en los ocho pilares de transformación existencial descritos por las Escuelas Ancestrales del Yin-Yang.",
+	openGraph: {
+		title: "Escuela de Libertad",
+		description:
+			"La primera Escuela de realización espiritual, basada en los ocho pilares de transformación existencial descritos por las Escuelas Ancestrales del Yin-Yang.",
+		images: [
+			{
+				url: "https://escueladelibertad.com/Assets/logosd.png",
+				width: 768,
+				height: 1024,
+				alt: "logo",
+			},
+		],
+		siteName: "Escuela de Libertad",
+	},
 };
 
 export default function RootLayout({ children }) {
