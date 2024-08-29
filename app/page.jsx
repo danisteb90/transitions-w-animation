0bdc570dd2a5b774a6cae698cdf28e28a74c233b
Esteban Alban <esteban.alban9003@gmail.com>
2024-08-29 13:04:49 -0500

    creacion nueva rama para revision sin afectar web principal

--- app/page.jsx
+++ app/page.jsx
@@ -4,11 +4,11 @@ import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 export default function Home() {
-	const router = useRouter();
+	// const router = useRouter();
 
-	useEffect(() => {
-		router.push("/diplomado-meditacion");
-	}, [router]);
+	// useEffect(() => {
+	// 	router.push("/diplomado-meditacion");
+	// }, [router]);
 
 	return (
 		<div>
