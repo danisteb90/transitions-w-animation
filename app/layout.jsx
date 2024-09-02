f64126942dd272362baf7cf2e96691a6e4117dd4
Esteban Alban <esteban.alban9003@gmail.com>
2024-09-02 10:43:53 -0500

    Added Speed Insights, Analytics and Pnpm

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,8 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Script from "next/script";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -59,6 +61,8 @@ export default function RootLayout({ children }) {
 			</head>
 			<body className={inter.className}>
 				<main>{children}</main>
+				<SpeedInsights />
+				<Analytics />
 			</body>
 		</html>
 	);
